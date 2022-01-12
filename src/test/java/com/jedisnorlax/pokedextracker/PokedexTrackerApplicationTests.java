626f4facc5af49d5c55c61e26422da5c8f3021d3
Jonathan Davis <cartman61616@gmail.com>
2022-01-12 15:48:20 -0500

    initial commit

---
+++ src/test/java/com/jedisnorlax/pokedextracker/PokedexTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jedisnorlax.pokedextracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
